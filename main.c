009f1262c3aa3b101ac74d0367ca9c97fe52b2af
Subhajit Patra <1souvikpa@gmail.com>
2020-10-16 21:32:41 +0530

    while loop

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 
 int main()
 {
-    int a,x;
-    printf("tell us the limit.\n");
-    scanf("%d",&x);
-    for(a=1;a<x;a=a+1){
-        for(int b=1;b<a;b=b+1){
-          printf("%d",a);
+    int a;
+    a=1;
+    while(a<=7){
+        int b=1;
+        while(b<a){
+            printf("%d",a);
+            b++;
         }
         printf("%d\n",a);
+        a++;
     }
+
     return 0;
 }
